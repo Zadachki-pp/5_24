1737a0c0eec9a45f7b606f9b716db84f9c6bb02d
LEv145 <you@example.com>
2024-02-15 19:40:52 +0200

    Update code

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <cmath>
 
 
-template <typename T, size_t N, size_t M>
+template <typename T, size_t M, size_t N>
 class Matrix {
     T** _matrix;
 
@@ -21,7 +21,7 @@ class Matrix {
     }
 
     void _copy_matrix_from(T** matrix) {
-        for(size_t i = 0; i < M; i++){
+        for(size_t i = 0; i < M; i++) {
             for(size_t j = 0; j < N; j++) {
                 _matrix[i][j] = matrix[i][j];
             }
@@ -41,7 +41,7 @@ public:
         _copy_matrix_from(other._matrix);
     }
 
-    Matrix<T, N, M>& operator=(const Matrix<T, N, M>& other) {
+    Matrix<T, M, N>& operator=(const Matrix<T, M, N>& other) {
         if (this == &other) {
             return *this;
         }
@@ -63,7 +63,7 @@ public:
         }
     }
 
-    Matrix<T, N, M>& operator+=(const Matrix<T, N, M>& other) {
+    Matrix<T, M, N>& operator+=(const Matrix<T, M, N>& other) {
         for(size_t i = 0; i < M; i++){
             for(size_t j = 0; j < N; j++) {
                 _matrix[i][j] += other._matrix[i][j];
@@ -73,111 +73,95 @@ public:
         return *this;
     }
 
-    template <size_t H>
-    Matrix<T, N, H> operator*=(const Matrix<T, M, H>& other) {
-        Matrix<T, N, H> result{};
-
+    Matrix<T, M, N> operator*=(T number) {
         for(size_t i = 0; i < M; i++){
             for(size_t j = 0; j < N; j++) {
-                for(size_t k = 0; k < H; k++) {
-                    _matrix[i][j] * other._matrix[i];
-                }
+                _matrix[i][j] *= number;
             }
         }
 
-        *this = result;  // WTF
-
         return *this;
+    };
+
+    template <size_t H>
+    Matrix<T, M, H> operator*(Matrix<T, N, H>& other) {
+        Matrix<T, M, H> new_matrix;
+
+        for(size_t k = 0; k < H; k++) {
+            for(size_t i = 0; i < M; i++) {
+                T item{};
+                for(size_t j = 0; j < N; j++) {
+                    item += _matrix[i][j] * other(j, k);
+                }
+                new_matrix(i, k) = item;
+            }
+        }
+
+        return new_matrix;
     }
 
-    Matrix<T, N, M> operator+(const Matrix<T, N, M>& other) {
+    Matrix<T, M, N> operator+(const Matrix<T, M, N>& other) {
         Matrix copy(*this);
         copy += other;
 
         return copy;
     }
 
-    template <size_t H>
-    Matrix<T, N, H> operator*(const Matrix<T, M, H>& other) {
-        Matrix copy(*this);
-        copy *= other;
-
-        return copy;
-    }
-
-    long long det() {
-        if (N != M){
+    int64_t det() {
+        if (M != N){
+            std::cerr << "The matrix is not square" << std::endl;
+            exit(1);
+        }
+        if (M > 3){
+            std::cerr << "The method is not implemented for matrices of this size" << std::endl;
             exit(1);
         }
 
-        long long sum = 0;
-        for(size_t j = 0; j < N; j++) {
-            sum += (
-                (long long)pw(j) * _matrix[0][j]
-                * get_additional_minor<N-1>(0, j)
+        if (M == 1) {
+            return _matrix[0][0];
+        }
+        else if (M == 2) {
+            return (
+                (_matrix[0][0] * _matrix[1][1])
+                - (_matrix[0][1] * _matrix[1][0])
             );
-
         }
-        return sum;
-    }
-
-    short pw(size_t x) {
-        if (x%2 == 0) return 1;
-        else return -1;
-    }
-
-    template<size_t K>
-    size_t get_additional_minor(size_t delete_i, size_t delete_j) {
-        Matrix<T, K, K> result{};
-
-        size_t dynamic_i = 0;
-        size_t dynamic_j = 0;
-        for(size_t i = 0; i < N; i++){
-            if (i == delete_i) continue;
-
-            for(size_t j = 0; j < N; j++) {
-                if (j == delete_j) continue;
-
-                result(dynamic_i, dynamic_j) = _matrix[i][j];
-
-                dynamic_j++;
-            }
-            dynamic_i++;
+        else {
+            return (
+                  (_matrix[0][0] * _matrix[1][1] * _matrix[2][2])
+                - (_matrix[0][0] * _matrix[1][2] * _matrix[2][1])
+                - (_matrix[0][1] * _matrix[1][0] * _matrix[2][2])
+                + (_matrix[0][1] * _matrix[1][2] * _matrix[2][0])
+                + (_matrix[0][2] * _matrix[1][0] * _matrix[2][1])
+                - (_matrix[0][2] * _matrix[1][1] * _matrix[2][0])
+            );
         }
-
-        std::cout << result << std::endl;
-
-        return result.det();
-    }
-
-    template<>
-    size_t get_additional_minor<0>(size_t delete_i, size_t delete_j) {
-        return _matrix[0][0];
     }
 
     void randomize() {
-        for (size_t i = 0; i < N; ++i) {
-            for (size_t j = 0; j < M; ++j) {
+        for (size_t i = 0; i < M; i++) {
+            for (size_t j = 0; j < N; j++) {
                 _matrix[i][j] = (rand() % 9) + 1;
             }
         }
     }
 };
 
-template <typename T, size_t N, size_t M>
-std::ostream& operator<<(std::ostream& out, Matrix<T, N, M>& matrix) {
-    for(size_t i = 0; i < M; i++){
+template <typename T, size_t M, size_t N>
+std::ostream& operator<<(std::ostream& out, Matrix<T, M, N>& matrix) {
+    for(size_t i = 0; i < M; i++) {
         for(size_t j = 0; j < N; j++) {
             out << matrix(i, j) << " ";
         }
-        out << std::endl;
+
+        if (i != M-1) out << std::endl;
     }
     return out;
 }
 
 
-template <typename T, size_t N, size_t M>
-std::istream& operator>>(std::istream& in, Matrix<T, N, M>& matrix) {
+template <typename T, size_t M, size_t N>
+std::istream& operator>>(std::istream& in, Matrix<T, M, N>& matrix) {
     for(size_t i = 0; i < M; i++){
         for(size_t j = 0; j < N; j++) {
             std::cin >> matrix(i, j);
@@ -186,15 +170,44 @@ std::istream& operator>>(std::istream& in, Matrix<T, N, M>& matrix) {
     return in;
 }
 
-
 int main() {
-    Matrix<int, 3, 3> matrix{};
-    matrix.randomize();
-    std::cout << matrix << std::endl;
-    std::cout << matrix.det() << std::endl;
-//    std::cin >> matrix;
-//    matrix++;
-//    std::cout << matrix;
+    Matrix<int, 2, 3> matrix1{};
+    matrix1.randomize();
+
+    Matrix<int, 3, 3> matrix2{};
+    matrix2.randomize();
+
+    std::cout << matrix1 << std::endl;
+    std::cout << std::endl;
+    std::cout << matrix2 << std::endl;
+    std::cout << std::endl;
+    std::cout << matrix2 << std::endl;
+    std::cout << std::endl;
+
+
+    std::cout << "Multiply (*)" << std::endl;
+    Matrix<int, 2, 3> matrix3 = matrix1 * matrix2;
+    std::cout << matrix3 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Det" << std::endl;
+    std::cout << matrix2.det() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Operators" << std::endl;
+    matrix1 += matrix3;
+    std::cout << matrix1 << std::endl;
+    std::cout << std::endl;
+    matrix1++;
+    std::cout << matrix1 << std::endl;
+    std::cout << std::endl;
+    matrix1 *= 2;
+    std::cout << matrix1 << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Get and write" << std::endl;
+    matrix1(0, 0) = 0;
+    std::cout << matrix1(0, 0) << " " << matrix1(1, 0) << std::endl;
 
     return 0;
 }
